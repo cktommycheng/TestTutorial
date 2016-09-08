178cbe11d2a67cba263bfe855e0590bc25b87911
cktommycheng <Tshark@Tommys-MacBook-Pro.local>
2016-09-08 11:17:22 -0400

    test file

---
+++ temp.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+y = 1
+
+if y ==1 :
+    print ("Hello")
+    
+    
+list = [2, 4, 6, 8]
+sum = 0
+for num in list: 
+    sum = sum + num
+    print(num, sum)
+print ("The sum is:", sum)
+
+for i in list: 
+    print(i)
