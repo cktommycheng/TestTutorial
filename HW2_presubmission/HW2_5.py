a9cb8ee567c01084cf92614de1849084f19ec4e5
cktommycheng <Tshark@Tommys-MacBook-Pro.local>
2016-09-22 11:41:16 -0400

    update

--- HW2_presubmission/HW2_5.py
+++ HW2_presubmission/HW2_5.py
@@ -1,52 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 21 13:25:45 2016
+Created on Thu Sep 22 10:39:57 2016
 
 @author: Tshark
 """
 
-co=[1,2,5,10,50,100,200]
-comb=[]
 
-def iter_fun(sum1, myString, Total):	
-	if sum1>Total:
-		return None	
-	#print(myString)
-	if sum1==Total:
-		myseq=myString.split()
-		myseq=[int(x) for x in myseq]
-		print(myseq)
-		myseq.sort()
-		comb.append(myseq)
-	for i in co:
-		iter_fun(sum1+i,myString + str(i)+' ',Total) 
+# S is the array of denominations that are available
+# m : number of denominations
+# n : amount that needs to be calculated
+# ans is the string that holds the combination of denomination
 
-def fixed_sum_digits(Tot):
-	iter_fun(0,"",Tot)
-	#print(comb)
-	comb.sort()
-	l=len(comb)
-	i=1
-	while i<l:
-		if comb[i-1]==comb[i]:
-			comb.pop(i)
-			i-=1
-			l-=1
-		i+=1
-	
-	#print(comb)
-	print(str(len(comb)) + " combinations are possible")
-	for i in comb:
-		lc=len(co)
-		n1=[0]*lc
-		for j in i:
-			n1[co.index(j)]+=1
-		pr=''
-		for i in range(lc):
-			pr = pr + str('\t') + str(co[i]) + str('*') + str(n1[i]) + str(' ')
-		print(pr)
+def count(S, m, n , ans) :
+  
+  	# base case of the recursion
+	if (n == 0) :
+		# uncomment the below line to print all the combinations also
+		# print(ans)
+		return 1
+	# 
+	if (n < 0) :
+		return 0
+ 
+
+	if (m <=0 and n >= 1) :
+		return 0
+
+	temp = ''
+	if(ans == '') :
+		temp = ''
+	else :
+		temp = ans + "+"
+
+	temp += str(S[m-1])
+
+	# recursion step
+	return count( S, m - 1, n ,ans) + count( S, m, n-S[m-1],temp)
+
+ 
 
 
-				
+arr = [1, 2, 5, 10, 20, 50, 100, 200]
+m = len(arr)
 
-fixed_sum_digits(200)
+print("TOTAL WAYS :: " + str(count(arr, m, 200, "")));
--- HW2_presubmission/HW2_8.py
+++ HW2_presubmission/HW2_8.py
@@ -1,52 +1,75 @@
-# calculating f(x) using the first expression
-# f(x) = 3.95*x*(1-x)
-def f1(x, n=1) :
-	# check the domain of x, else raise an error
-	if(x > 1 or x < 0) :
-		print("Invalid value of x !")
-		return None
-	else :
-		# recursive step
-		if(n > 1) :
-			return f1(3.95*x - 3.95*(x**2), n-1)
-		# base case
-		else :
-			return 3.95*x*(1-x)
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 10:23:08 2016
+
+@author: Tshark
+"""
+'''
+Question 8: This program prompts user for an input of an integer and determines
+whether it is a prime number 
+
+'''
+
+def fun1(x,n):
+    """
+    the function calculates the value of f(x)=3.95*（x-x**2）with n times recursion
+    parameters:
+    x: a number in [0,1] 
+    n: numbers of recursion 
+    """
+    
+    if n==1:        #if n=1, return base case
+        return 3.95*(x-x**2) 
+    else:
+        return fun1(fun1(x,1),n-1) # if n!= 1, then take the value from previous iteration into the next iteration
+
+def fun2(x,n):
+    """
+    the function calculuates the value function f(x)=3.95*x*(1-x）through n times recursion
+    
+    parameters:
+    x: a number in [0,1] 
+    n: number of recursion 
+    """
+    if n==1:     #if n=1, return base case
+        return 3.95*x*(1-x) 
+    else:
+        return fun2(fun2(x,1),n-1) # if n!= 1, then take the value from previous iteration into the next iteration
 
 
-
-# calculating f(x) using the first expression
-# f(x) = 3.95*x - 3.95*x*x
-def f2(x, n=1) :
-	# check the domain of x, else raise an error
-	if(x > 1 or x < 0) :
-		print("Invalid value of x !")
-		return None
-	else :
-		# recursive step
-		if(n > 1) :
-			return f2(3.95*x - 3.95*(x**2), n-1)
-		# base case
-		else :
-			return 3.95*x - 3.95*(x**2)
+def fun3(x,n):
+    """
+    the function calculates the value of f(x)=3.95*x-3.95*(x**2) through n times recursion
+    
+    parameters:
+    x: a number in [0,1] 
+    n: number of recursion 
+    """
+    if n==1:
+        return 3.95*x-3.95*(x**2) #the end of recursion，also is the first time to cacalute the result
+    else:
+        return fun3(fun3(x,1),n-1) # if n!= 1, then take the value from previous iteration into the next iteration
 
 
-
-# calculating f(x) using the first expression
-# f(x) = 3.95*(x-x*x)
-def f3(x, n=1) :
-	# check the domain of x, else raise an error
-	if(x > 1 or x < 0) :
-		print("Invalid value of x !")
-		return None
-	else :
-		# recursive step
-		if(n > 1) :
-			return f3(3.95*x - 3.95*(x**2), n-1)
-		# base case
-		else :
-			return 3.95*(x-(x**2))
+#Test Case: n=1, n = 10, n = 50, n = 100
+print("When x = 0.5 , n = 1")
+print("the answers return: ", fun1(0.5, 1), fun2(0.5, 1), fun3(0.5,1) )
+print("When x = 0.5 , n = 2")
+print("the answers return: ", fun1(0.5, 2), fun2(0.5, 2), fun3(0.5,2) )
+print("When x = 0.5 , n = 10")
+print("the answers return: ", fun1(0.5, 10), fun2(0.5, 10), fun3(0.5,10) )
+print("When x = 0.5 , n = 50")
+print("the answers return: ", fun1(0.5, 50), fun2(0.5, 50), fun3(0.5,50) )
+print("When x = 0.5 , n = 100")
+print("the answers return: ", fun1(0.5, 100), fun2(0.5, 100), fun3(0.5,100))
+print("When x = 0.9 , n = 100")
+print("the answers return: ", fun1(0.9, 100), fun2(0.9, 100), fun3(0.9,100))
 
 
-
-print(f1(0.9, 100), f2(0.9, 100), f3(0.9, 100))
+"""
+Comments on the result:
+As we can see, wehn the iterations go up, the answers produced by the 3 functions are
+different. This is because there is a boundary on storing decimal places. As the iterations go up.
+the decimals are rounded off. So each iteration contains round off error. AS the iterations go up to 
+100, the round off errors accumluate, which produce different values from the 3 functions 
+"""
