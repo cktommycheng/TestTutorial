ddc39e9c23c7c49c00ede8bf76580f9d6aa731fb
cktommycheng <Tshark@Tommys-MacBook-Pro.local>
2016-09-22 17:13:45 -0400

    update

--- HW2_presubmission/HW2_7.py
+++ HW2_presubmission/HW2_7.py
@@ -1,43 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 21 13:23:49 2016
+Created on Thu Sep 22 17:10:53 2016
 
 @author: Tshark
 """
 
-def sort(lst, offset = None) :
+'''Quesiton 7'''   
+def sort(list):
+    '''
+    the function, sort(list), accepts a list of strings and uses recursion to 
+    return a sorted list. Each step should at most modify two elements of the list.
+    parameters:
+    list- a list of strings
+    
+    return
+    a list of sorted strings
+    '''
+    tem=list[0] #initialize a temporary string which represents biggest string 
+    for i in range(1,len(list)):
+        if list[i]>tem:
+            tem=list[i]# read the strings one by one and give the biggest one to tem
+        else:
+            continue
+    list.remove(tem)# remove the biggest one from list
+    if list==[]:
+        return [tem]# recursion end condition: if only one string left, return this one-string list
+    else:
+        return sort(list)+[tem]# new list will be re-sorted from small to big according to the ASCII code
 
-	# set the offset to the last index of lst
-	if(offset is None) :
-		offset = len(lst) - 1
-	
-	# checking for the base case of the recursion. i.e, to stop iterating when offset has reached 0
-	if(offset >= 0) :
-		# recursive step
-		lst = sort(lst, offset - 1)
-		
-		# set the value of ele to the offset, we assume this to be the smallest element
-		ele = offset
-
-		# for loop to find the smallest element in range (offset, len(lst))
-		for i in range(offset + 1, len(lst)) :
-			# change the smallest element index i.e. ele , if smaller element is encountered
-			if(lst[ele] > lst[i]) :
-				ele = i
-
-		# swap the element at offset with that at ele
-		tmp = lst[offset]
-		lst[offset] = lst[ele]
-		lst[ele] = tmp
-
-		# print the lst at that point
-		print("\n\nSTEP " + str(offset + 1) + " : ")
-		for i in range(0, len(lst)) :
-			print(lst[i])
-
-	return lst
-
-
-
-data = ["new york", "san fransisco", "delhi", "sydney", "london", "paris", "rome", "los angeles"]
-sort(data)
+sort(['you','are','good','person'])
