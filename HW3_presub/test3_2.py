010a5e79179d3a7105056129125a559c3775df7a
cktommycheng <Tshark@Tommys-MacBook-Pro.local>
2016-10-13 15:01:57 -0400

    su

--- HW3_presub/test3_2.py
+++ HW3_presub/test3_2.py
@@ -8,16 +8,33 @@ Created on Thu Sep 22 19:08:59 2016
 import matplotlib.pyplot as plot
 import pandas as pd
 import numpy as np
+import math
+import gzip
+import urllib.request
+import os 
+
+    
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/census-income-mld/census-income.data.gz"
+ftpstream = urllib.request.urlopen(url)
+census_cols = ['AAGE','ACLSWKR','ADTIND','ADTOCC','AGI','AHGA','AHRSPAY','AHSCOL','AMARITL','AMJIND','AMJOCC','ARACE',
+               'AREORGN','ASEX','AUNMEM','AUNTYPE','AWKSTAT','CAPGAIN','CAPLOSS','DIVVAL','FEDTAX','FILESTAT','GRINREG',
+               'GRINST','HHDFMX','HHDREL','MARSUPWT','MIGMTR1','MIGMTR3','MIGMTR4','MIGSAME','MIGSUN','NOEMP','PARENT',
+               'PEARNVAL','PEFNTVTY','PEMNTVTY','PENATVTY','PRCITSHP','PTOTVAL','SEOTR','TAXINC','VETQVA','VETYN','WKSWORK']
+with gzip.open(ftpstream) as KDD:
+    census = pd.read_csv(KDD, names = census_cols)
 
 
 
 ab_cols = ['Sex', 'Length', 'Diameter', 'Height', 'WholeWt', 'ShuckedWt', 'VisceraWt', 'ShellWt', 'Rings']
 abalone = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data", names = ab_cols)
 diamonds = pd.read_csv('https://vincentarelbundock.github.io/Rdatasets/csv/Ecdat/Diamond.csv', skiprows=1, names= ['carat', 'colour','clarity','certification', 'price'])
+#KDD = pd.read_csv("KDD.txt")
+
+
 diamonds_num = diamonds.ix[:,0]
 abalone_num = abalone.ix[:,1]
 
-#separate out the set of the numerical column from the dataset diamonds
+#separate out the set of the numerical columns from the dataset diamonds
 carats = diamonds.ix[:,0]
 price = diamonds.ix[:,4]
 
@@ -32,6 +49,9 @@ shuckedWt = abalone.ix[:,5]
 visceraWt = abalone.ix[:,6]
 shellWt = abalone.ix[:,7]
 rings = abalone.ix[:,8]
+
+#separate out the set of numerical columns from the dataset KDD
+ages = KDD.ix[:,0]
 
 
 
@@ -71,11 +91,13 @@ def split_dist(data):
     plot.figure()
     data.plot.box()
     
-    
 
 
 
-split_dist(shellWt)
-print(shellWt)
+
+
+#split_dist(ages)
+#split_dist(shellWt)
+#print(shellWt)
 
 
